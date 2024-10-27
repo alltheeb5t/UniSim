0bae7bb89faeda0fb92b233e4cc49cff3ded42b2
Will Hall <will@limeparallelogram.uk>
2024-10-27 15:03:32 +0000

    Mouse-based map movement implemented.

--- core/src/main/java/io/github/alltheeb5t/unisim/GameScreen.java
+++ core/src/main/java/io/github/alltheeb5t/unisim/GameScreen.java
@@ -2,6 +2,7 @@ package io.github.alltheeb5t.unisim;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -9,6 +10,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -18,7 +20,7 @@ import com.badlogic.gdx.utils.viewport.Viewport;
 
 import io.github.alltheeb5t.unisim.map_objects.MapBuilding;
 
-public class GameScreen extends ScreenAdapter {
+public class GameScreen extends ScreenAdapter implements InputProcessor {
 
     private OrthographicCamera camera;
     private SpriteBatch batch;
@@ -30,6 +32,11 @@ public class GameScreen extends ScreenAdapter {
 
     private DragAndDrop dragAndDrop;
 
+    // Used by to track how much the cursor has moved between two 'touchDragged' events.
+    // The camera is then translated by this.
+    private int panStartX;
+    private int panStartY;
+
     public GameScreen (OrthographicCamera camera, Viewport viewport) {
         this.camera = camera;
         this.viewport = viewport;
@@ -39,7 +46,7 @@ public class GameScreen extends ScreenAdapter {
         box2dDebugRenderer = new Box2DDebugRenderer();
         stage = new Stage(viewport);
         dragAndDrop = new DragAndDrop();
-        Gdx.input.setInputProcessor(stage);
+        Gdx.input.setInputProcessor(this); // Inputs related to drag are manually passed to stage
 
         // Testing only: Add temporary building
         testBuilding = new MapBuilding(480, 100, 120, world, new Texture("piazza.png"));
@@ -66,8 +73,6 @@ public class GameScreen extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
-        handleInput();
-
         Gdx.gl.glClearColor(118/255f,184/255f, 135/255f,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -82,30 +87,11 @@ public class GameScreen extends ScreenAdapter {
     }
 
     /** 
-     * Handles inputs related to zoom. Called exclusively by the render method.
+     * Moves the camera by a set amount but prevents going beyond map boundaries
      */
-    private void handleInput() {
-		if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-			camera.zoom += 0.02;
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
-			camera.zoom -= 0.02;
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			camera.translate(-3, 0, 0);
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			camera.translate(3, 0, 0);
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-			camera.translate(0, -3, 0);
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-			camera.translate(0, 3, 0);
-		}
-
-        // Minimum zoom is the entire viewport filling the screen and the maximum is an arbitrary 0.1 (On a 1080p monitor this would be ~98m)
-		camera.zoom = MathUtils.clamp(camera.zoom, 0.1f, Math.min(Constants.CAMPUS_MAX_X/camera.viewportWidth, Constants.CAMPUS_MAX_Y/camera.viewportHeight));
+    private void clampedTranslate(float x, float y) {
+        camera.position.x += x;
+        camera.position.y += y;
 
 		float effectiveViewportWidth = camera.viewportWidth * camera.zoom;
 		float effectiveViewportHeight = camera.viewportHeight * camera.zoom;
@@ -123,5 +109,93 @@ public class GameScreen extends ScreenAdapter {
         camera.viewportHeight = height;
         camera.update();
     }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        panStartX = screenX;
+        panStartY = screenY;
+        return stage.touchDown(screenX, screenY, pointer, button);
+    }
+
+    /**
+     * Implemented from InputProcessor. Used to handle panning if middle click is pressed
+     * @param screenX Pointer X coordinate
+     * @param screenY Pointer Y coordinate
+     * @param pointer Think this is always 0 for a mouse.
+     * @return
+     */
+    @Override
+    public boolean touchDragged (int screenX, int screenY, int pointer) {
+        if (Gdx.input.isButtonPressed(Input.Buttons.MIDDLE))  {
+            clampedTranslate((panStartX-screenX), -(panStartY-screenY));
+            panStartX = screenX;
+            panStartY = screenY;
+        }
+        
+        // Input handler would ordinarily be set to the stage. Now we've finished, pass input events to stage.
+        return stage.touchDragged(screenX, screenY, pointer);
+    }
+
+    @Override
+    public boolean touchUp (int screenX, int screenY, int pointer, int button) {
+        return stage.touchUp(screenX, screenY, pointer, button);
+    }
+
+	@Override public boolean keyDown (int keycode) {
+		return false;
+	}
+
+	@Override public boolean keyUp (int keycode) {
+		return false;
+	}
+
+	@Override public boolean keyTyped (char character) {
+		return false;
+	}
+
+    /**
+     * Implemented from InputProcessor, used to control Zoom.
+     * @param amountX Arbitrary value representing how far the window should move.
+     * @param amountY Would be horizontal scrolling. Not used.
+     * @return
+     */
+	@Override public boolean scrolled (float amountX, float amountY) {
+        // We find the in-game coordinates of the cursor initially
+        Vector3 cursorPosMeters = camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+        camera.zoom += amountY/10;
+
+        // Minimum zoom is the entire viewport filling the screen and the maximum is an arbitrary 0.1 (On a 1080p monitor this would be ~98m)
+		camera.zoom = MathUtils.clamp(camera.zoom, 0.1f, Math.min(Constants.CAMPUS_MAX_X/camera.viewportWidth, Constants.CAMPUS_MAX_Y/camera.viewportHeight));
+
+        camera.update();
+
+        // Get the in-game coordinates of the cursor now the zoom has been executed
+        Vector3 newCursorPosMeters = camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+        
+        // To make zoom natural, whatever is directly under the cursor should stay in the same place
+        clampedTranslate(cursorPosMeters.x-newCursorPosMeters.x,cursorPosMeters.y-newCursorPosMeters.y);
+        camera.update();
+		return true;
+	}
+
+    /**
+     * Implemented as is necessary for a full implementation of InputProcessor. Not relevant on desktop
+     * @param screenX
+     * @param screenY
+     * @param point
+     * @param button
+     * @return
+     */
+    @Override
+    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+
     
 }
